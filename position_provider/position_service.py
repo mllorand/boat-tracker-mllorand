54f45d279a2e6e9eafcac14f33b9472073ca8d65
pitchforx <m.lorandlevente@gmail.com>
2022-11-21 14:11:49 +0100

    Define function to convert line data to iterator with generator expression

--- position_provider/position_service.py
+++ position_provider/position_service.py
@@ -8,6 +8,15 @@ sio = socketio.Server(cors_allowed_origins="*", async_mode='eventlet')
 app = socketio.WSGIApp(sio)
 
 
+def iter_from_data_frame(line_data):
+    return ({
+        'lat': line_data['lat'][i],
+        'lon': line_data['lon'][i],
+        'heading': line_data['heading'][i]
+    } for i in range(len(line_data))
+    )
+
+
 @sio.event
 def connect(sid, environ):
     print(sid, 'connected')
